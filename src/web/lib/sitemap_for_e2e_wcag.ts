91f3fbf18dc3df5a71e2501857adc14cead0c081
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-11-15 08:52:39 -0600

    Whoopsie, never meant for that draft to sneak in.

--- src/web/lib/sitemap_for_e2e_wcag.ts
+++
@@ -1,9 +0,0 @@
-export const routes: Routes = [
-    {
-        path: "",
-        component: LandingComponent,
-        data: {
-            loc: "/",
-            lastmod: "2022-04-07",
-            changefreq: "daily",
-            priority: "1.0",
