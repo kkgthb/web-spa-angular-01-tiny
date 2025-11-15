88db3574cceb81857950a08d3ffb8fea42cade82
kkgthb <44843658+kkgthb@users.noreply.github.com>
2025-11-15 08:16:39 -0600

    Added some dynamic URL-based routing, too, to make it a little harder to decide what to do about sitemap generation.

--- src/web/src/app/app.routes.ts
+++ src/web/src/app/app.routes.ts
@@ -5,5 +5,8 @@ import { PageNotFoundComponent } from "./public/page-not-found/page-not-found.co
 export const routes: Routes = [
     { path: '', component: GenericPageComponent, pathMatch: 'full' },
     { path: 'about', component: GenericPageComponent, data: { genericPageName: 'About Us' } },
+    { path: 'dynamic_path_based', component: GenericPageComponent, data: { genericPageName: 'Parent catch-all for Dynamic (path-based)', pathBased: false, } },
+    { path: 'dynamic_path_based/:slug', component: GenericPageComponent, data: { genericPageName: 'Dynamic (path-based)', pathBased: true, } },
+    { path: 'dynamic_query_based', component: GenericPageComponent, data: { genericPageName: 'Dynamic (query-parameter-based)', queryBased: true, } },
     { path: '**', component: PageNotFoundComponent },
 ]
--- src/web/src/app/public/generic-page/generic-page.component.ts
+++ src/web/src/app/public/generic-page/generic-page.component.ts
@@ -13,8 +13,27 @@ import { GreetingComponent } from 'app/features/greeting/greeting.component';
 })
 export class GenericPageComponent {
     private route = inject(ActivatedRoute);
-    genericPageName = computed(() => {
-        console.log(this.route.snapshot.data);
-        return this.route.snapshot.data['genericPageName'] || 'Fallback';
+    genericPageName = computed((): string => {
+        const genericPageNameDataInput: string = this.route.snapshot.data['genericPageName'];
+        console.log(genericPageNameDataInput);
+        if (!genericPageNameDataInput) {
+            // Short-circuit to "Fallback" if there was no "genericPageName" data input to begin with.
+            return 'Fallback';
+        }
+        if (!!this.route.snapshot.data['pathBased']) {
+            // Amend generic page name to include "slug" route parameter value, if applicable
+            const routeParamSlug = this.route.snapshot.paramMap.get('slug');
+            if (!!routeParamSlug) {
+                return `${genericPageNameDataInput} -- ${routeParamSlug}`;
+            }
+        }
+        if (!!this.route.snapshot.data['queryBased']) {
+            // Amend generic page name to include "now_on" query parameter value, if applicable
+            const routeQueryNowOn = this.route.snapshot.queryParamMap.get('now_on');
+            if (!!routeQueryNowOn) {
+                return `${genericPageNameDataInput} -- ${routeQueryNowOn}`;
+            }
+        }
+        return genericPageNameDataInput;
     });
 }
